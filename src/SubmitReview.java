c0a46845617677acdf8117101af0c27e9f8cda8d
“Yash <Yash.chauhan@infostride.com>
2024-06-28 19:27:18 +0530

    nopCommerce

--- src/SubmitReview.java
+++ src/SubmitReview.java
@@ -6,8 +6,8 @@ public class SubmitReview extends NopCommerce_Login{
 
     public void writeReview(){
         Actions action = new Actions(driver);
-        action.moveToElement(driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/div/div/div[4]/div[2]/div[1]/div"))).click().perform();
-        action.moveToElement(driver.findElement(By.xpath("//*[@id=\"review-form\"]/form/div[1]/div"))).perform();
+        action.moveToElement(driver.findElement(By.xpath("//div[@data-productid=\"1\"]"))).click().perform();
+        action.moveToElement(driver.findElement(By.xpath("//div[@class=\"form-fields\"]"))).perform();
         driver.findElement(By.xpath("//input[@class=\"review-title\"]")).sendKeys("Testing review");
         driver.findElement(By.xpath("//textarea[@class=\"review-text\"]")).sendKeys("Testing Comment field");
         driver.findElement(By.xpath("//input[@value=\"5\"]")).click();
